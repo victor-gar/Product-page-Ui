c54de9cc6a8c1cf2db7622cb56e0839f9dddabc6
Victor Garitskyu <victor_95_22@mail.ru>
2021-08-04 08:07:49 +0600

    Initial Commit

---
+++ Complex Ui/Complex_UiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Complex_UiApp.swift
+//  Complex Ui
+//
+//  Created by Victor Garitskyu on 04.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct Complex_UiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
